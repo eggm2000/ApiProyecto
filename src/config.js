dc603528df47fde89434c9c23f7d813516865904
eggm2000 <galdeaerika7@gmail.com>
2024-12-05 02:56:03 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 
-export const BD_HOST = process.env.BD_HOST || "127.0.0.1";
+export const BD_HOST = process.env.BD_HOST || "bhxbetaqp8tp9dfpanjm-mysql.services.clever-cloud.com";
 export const BD_DATABASE = process.env.BD_DATABASE || "Proyecto";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
