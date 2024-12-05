822cea80c2f5b9a96d1c2839456af7a718c43b4e
eggm2000 <galdeaerika7@gmail.com>
2024-12-05 02:45:44 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const BD_HOST = process.env.BD_HOST || "127.0.0.1";
-export const BD_DATABASE = process.env.BD_DATABASE || "inventario";
+export const BD_DATABASE = process.env.BD_DATABASE || "Proyecto";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_PORT = process.env.DB_PORT || 3306;
